b123f35941acd51f5bc8b0f5273c971758bcb0bc
Topi Kettunen <topi@kettunen.io>
2020-11-01 00:59:28 +0200

    Tweaks

--- tests/main.lisp
+++ tests/main.lisp
@@ -4,7 +4,7 @@
         :rove))
 (in-package :sv/tests/main)
 
-;; NOTE: To run this test file, execute `(asdf:test-system :sv)' in your Lisp.
+;; NOTE: To run this test file, execute `(asdf:test-system :serendipitous-vocable)' in your Lisp.
 
 (deftest test-target-1
   (testing "should (= 1 1) to be true"
